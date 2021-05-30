c2e9018e4337b44b22e729bafd1a3db65fc71875
qianjiachun <189964430@qq.com>
2021-05-31 00:38:54 +0800

    init

--- src/pages/spect/packages/Compare2D/views/Diff.vue
+++ src/pages/spect/packages/Compare2D/views/Diff.vue
@@ -1,5 +1,8 @@
 <template>
     <div>
+        <div style="position: absolute;">加载中...</div>
+        <div class="panel">
+        </div>
         <div style="filter: brightness(20);" ref="canvas1"></div>
     </div>
 </template>
@@ -8,6 +11,8 @@
 import { defineComponent, ref, getCurrentInstance, onMounted, nextTick } from "vue";
 import { getBase64 } from "@/src/utils/index"
 export default defineComponent({
+    components: {
+    },
     setup(props, ctx) {
         let {proxy} = getCurrentInstance();
         let img1Url = proxy.img1;
@@ -26,13 +31,10 @@ export default defineComponent({
         })
 
         const renderDiffImg = () => {
-            // console.log(params.value)
-            // return
             let img = new Image();
             img.src = img1.value;
             img.onload = () => {
                 let ratio = img.height / img.width;
-                console.log(img.width,img.height)
                 let width = document.getElementById("app").clientWidth;
                 let height = width * ratio;
                 document.getElementsByClassName("menu")[0].style.height = height;
@@ -70,38 +72,24 @@ export default defineComponent({
                     renderDiffImg();
                 }
             })
-
-            // let img = new Image();
-            // img.src = img1;
-            // img.onload = () => {
-            //     let ratio = img.height / img.width;
-            //     let width = document.getElementById("app").clientWidth;
-            //     let height = width * ratio;
-            //     document.getElementsByClassName("menu")[0].style.height = height;
-            //     nextTick(() => {
-            //         const promiseCompare = canvasCompare(params.value);
-
-            //         promiseCompare.then(function (result) {
-            //             // Do things with result
-            //             let imgDom = result.producePreview();
-            //             imgDom.width = width;
-            //             imgDom.height = height;
-            //             canvas1.value.appendChild(imgDom);
-            //         });
-
-            //         promiseCompare.catch(function (reason) {
-            //             // Handle the error
-            //         });
-            //     })
-            // }
-            
         })
         return {
-            canvas1
+            canvas1,
         };
     },
 });
 </script>
 
 <style scoped>
+.panel {
+    width: 250px;
+    height: 200px;
+    background-color: rgba(255, 255, 255, 0.7);
+    border-radius: 10px 10px 0px 0px;
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    z-index: 1;
+}
+
 </style>
