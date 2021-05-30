3d35ce3eb959d2d8989b7701ee201051081bb3b4
qianjiachun <189964430@qq.com>
2021-05-31 00:08:34 +0800

    init

---
+++ header.js
@@ -0,0 +1,18 @@
+// ==UserScript==
+// @name         CSGO饰品对比
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  try to take over the world!
+// @author       小淳
+// @match        *://buff.163.com/market/goods*
+// @match        *://spect.fp.ps.netease.com/*
+// @grant        GM_setValue
+// @grant        GM_getValue
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// @require      https://cdn.jsdelivr.net/npm/notice.js@0.4.0/dist/notice.js
+// @require      https://lib.baomitu.com/vue/3.0.11/vue.global.prod.js
+// @require      https://cdn.jsdelivr.net/npm/comparison-slider@1.1.0/dist/comparison-slider.min.js
+// @require      https://cdn.jsdelivr.net/npm/canvas-compare@3.0.0/src/canvas-compare.min.js
+// @require      file://D:\Data\code\csgo\csgo-skin-compare\dist\main.js
+// ==/UserScript==
